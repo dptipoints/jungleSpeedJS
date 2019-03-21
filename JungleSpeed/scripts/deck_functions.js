7177ba63718d70d1e905d49bad067242f5b500e3
Maxime Druart <maxime.druart@yahoo.fr>
2019-03-21 20:17:20 +0100

    nettoyage

--- JungleSpeed/scripts/deck_functions.js
+++ JungleSpeed/scripts/deck_functions.js
@@ -4,15 +4,15 @@ function deckGen() {
       let internalArray = []
       internalArray.push(shape, color)
       // internalArray.push(file) peut etre je sais pas
-      initialDeck.push(internalArray)
+      deck.push(internalArray)
     }
   }
   for (var i = 0; i < 3; i++) {
     for (var x = 0; x < 4; x++) {
-      initialDeck.push("special"+i)
+      deck.push("special"+i)
     }
   }
-  return initialDeck
+  return deck
 }
 function deckShuffle(deck) { // c'est du gros vol (Fisher-Yates shuffle)
   for (let i = deck.length - 1; i > 0; i--) {
@@ -40,4 +40,60 @@ function deckDistribution(deck, nb_players){ // on obtient une liste de longueur
     count++
   }
   return allPlayerDecks
+}
+
+
+
+function distribute(nbPlayer){
+
+  if (nbPlayer == 2) {
+    for (var i = 0; i < deck.length; i+=nbPlayer) {
+      player1Deck.push(deck[i])
+    }
+    for (var i = 1; i < deck.length; i+=nbPlayer) {
+      player2Deck.push(deck[i])
+    }
+  }
+
+  if (nbPlayer == 3) {
+    for (var i = 0; i < deck.length; i+=nbPlayer) {
+      player1Deck.push(deck[i])
+    }
+    for (var i = 1; i < deck.length; i+=nbPlayer) {
+      player2Deck.push(deck[i])
+    }
+    for (var i = 2; i < deck.length; i+=nbPlayer) {
+      player3Deck.push(deck[i])
+    }
+  }
+
+  if (nbPlayer == 4) {
+    for (var i = 0; i < deck.length; i+=nbPlayer) {
+      player1Deck.push(deck[i])
+    }
+    for (var i = 1; i < deck.length; i+=nbPlayer) {
+      player2Deck.push(deck[i])
+    }
+    for (var i = 2; i < deck.length; i+=nbPlayer) {
+      player3Deck.push(deck[i])
+    }
+    for (var i = 3; i < deck.length; i+=nbPlayer) {
+      player4Deck.push(deck[i])
+    }
+  }
+  deckList = [player1Deck, player2Deck, player3Deck, player4Deck]
+  deckListPlayed = [player1Played, player2Played, player3Played, player4Played]
+}
+
+function play(i){
+  deckListPlayed[i].push(deckList[i][deckList[i].length-1])
+  deckList[i].pop()
+  // AFFICHER LA DERNIERE CARTE DE PLAYER1PLAYED
+}
+
+
+function playerListCreation(nbPlayers){
+  if (nbPlayers == 2) {playerList = ["player1","player2"]}
+  if (nbPlayers == 3) {playerList = ["player1","player2","player3"]}
+  if (nbPlayers == 4) {playerList = ["player1","player2","player3","player4"]}
 }
--- JungleSpeed/scripts/main.js
+++ JungleSpeed/scripts/main.js
@@ -1,64 +1,22 @@
-let deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-let player1Deck = [], player2Deck = [], player3Deck = [], player4Deck = []
-let player1Played = [], player2Played = [], player3Played = [], player4Played = []
-let deckList = []
-let deckListPlayed = []
+let nbPlayersButtons = document.querySelectorAll(".playercount li")
+let overlay = document.querySelector(".overlayBeginning")
+let nbPlayers, deck = []
 
-let nbPlayer = parseInt(window.prompt("Combien de joueur ?", "4"))
-
-if (nbPlayer == 2) {
-  playerList = ["player1","player2"]
-}
-if (nbPlayer == 3) {
-  playerList = ["player1","player2","player3"]
-}
-if (nbPlayer == 4) {
-  playerList = ["player1","player2","player3","player4"]
+// 18*4 (72) cartes normales + 12 cartes spéciales
+let colors = ["yellow", "green", "blue", "red"]
+let shapes = [] // TODO: a nommer
+for (var i = 0; i < 18; i++) { // temporaire, le temps de nommer les formes
+  shapes.push("shape"+(i+1))
 }
 
-function distribute(nbPlayer){
-
-  if (nbPlayer == 2) {
-    for (var i = 0; i < deck.length; i+=nbPlayer) {
-      player1Deck.push(deck[i])
-    }
-    for (var i = 1; i < deck.length; i+=nbPlayer) {
-      player2Deck.push(deck[i])
-    }
-  }
-
-  if (nbPlayer == 3) {
-    for (var i = 0; i < deck.length; i+=nbPlayer) {
-      player1Deck.push(deck[i])
-    }
-    for (var i = 1; i < deck.length; i+=nbPlayer) {
-      player2Deck.push(deck[i])
-    }
-    for (var i = 2; i < deck.length; i+=nbPlayer) {
-      player3Deck.push(deck[i])
-    }
-  }
-
-  if (nbPlayer == 4) {
-    for (var i = 0; i < deck.length; i+=nbPlayer) {
-      player1Deck.push(deck[i])
-    }
-    for (var i = 1; i < deck.length; i+=nbPlayer) {
-      player2Deck.push(deck[i])
-    }
-    for (var i = 2; i < deck.length; i+=nbPlayer) {
-      player3Deck.push(deck[i])
-    }
-    for (var i = 3; i < deck.length; i+=nbPlayer) {
-      player4Deck.push(deck[i])
-    }
-  }
-  deckList = [player1Deck, player2Deck, player3Deck, player4Deck]
-  deckListPlayed = [player1Played, player2Played, player3Played, player4Played]
-}
-
-function play(i){
-  deckListPlayed[i].push(deckList[i][deckList[i].length-1])
-  deckList[i].pop()
-  // AFFICHER LA DERNIERE CARTE DE PLAYER1PLAYED
-}
+nbPlayersButtons.forEach((e, index) => {
+  e.addEventListener('click',() => {
+    overlay.classList.add("displayHide")
+    nbPlayers = index+2
+    playerListCreation(nbPlayers)
+    deckGen()
+    deckShuffle(deck)
+    deck = deckDistribution(deck, nbPlayers)
+    // FONCTION DISTRIBUTE DE EYTHAN
+  })
+})
